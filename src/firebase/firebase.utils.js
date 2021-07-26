1eadc1428243dac05969affc61be5e17b2e1a781
Ldeng928 <lideng928@gmail.com>
2021-07-26 15:04:32 -0400

    implemented firebase utilities, included ability to store, authenticated users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,13 +4,44 @@ import 'firebase/firestore';
 import 'firebase/auth'
 
 const config = {
-    apiKey: "AIzaSyBhVlsS1Rh2bRkRsD32RnaVi3vklrFKg20",
+    apiKey: "<Your API Key>",
     authDomain: "crwn-db-5f44b.firebaseapp.com",
     projectId: "crwn-db-5f44b",
     storageBucket: "crwn-db-5f44b.appspot.com",
     messagingSenderId: "93686599638",
     appId: "1:93686599638:web:b17d1a7739e04ea6199e77"
   };
+
+// Make an API request to get the 'uid' Google made.
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+  // check if there is an userAuth object
+  if(!userAuth) return;
+
+  // Snapshot reference
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  // If there is no such user exists in the database. Create a new one
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      // create method
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      }); 
+    } catch(error){
+      console.log('error creating user', error.message);
+    }
+  }
+
+  // Return userRef in case it is needed
+  return userRef;  
+}
 
 firebase.initializeApp(config);
 
