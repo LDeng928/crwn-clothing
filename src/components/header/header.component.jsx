e005a1018a332dafa6453a4f29ba2da254580725
Ldeng928 <lideng928@gmail.com>
2021-07-27 11:59:35 -0400

    cartIcon and cartDropdown components to application. Added new cart redux with hidden value

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import {auth} from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.style.scss';
 
 // functional component
-const Header = ({currentUser}) => (  
+const Header = ({currentUser, hidden}) => (  
     <div className="header">
         <Link className='logo-container' to='/'>
             <Logo className='logo'></Logo>
@@ -28,13 +30,19 @@ const Header = ({currentUser}) => (
                     <Link className='option' to='/signin'>SIGN IN</Link>
                 }
             </Link>
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />  
+        }
+            
     </div>
     );
 
 // Get state from redux
-const mapStatetoProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
-export default connect(mapStatetoProps)(Header);
+export default connect(mapStateToProps)(Header);
